c3393e160a48b1d0c5a1283e7e2478e29a529595
Md Sabbir Ahamed <52445722+Redoy0@users.noreply.github.com>
2026-06-06 01:53:49 +0600

    Time: 16 ms (29.14%), Space: 23.8 MB (31.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,9 @@
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        my_map=defaultdict(list) #add empty list into dictionary to append value easily
-        ans=[] # to store values of dictionary
+        result = defaultdict(list)
         for s in strs:
-            sorted_s=tuple(sorted(s)) #converted in tuple because dictionary use immutable key
-            my_map[sorted_s].append(s) #storing values for same sorted keys
-        for value in my_map.values():
-            ans.append(value) 
-        return ans
+            hashmap = [0] * 26
+            for c in s:
+                hashmap[ord(c)-ord("a")] +=1
+            result[tuple(hashmap)].append(s)
+        return list(result.values())
